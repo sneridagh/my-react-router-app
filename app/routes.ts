cff698563bd8fad4c7751ef196dc084a6f11561b
Victor Fernandez de Alba <sneridagh@gmail.com>
2025-02-24 16:39:38 +0100

    Update to make it fail properly

--- app/routes.ts
+++ app/routes.ts
@@ -1,3 +1,15 @@
-import { type RouteConfig, index } from "@react-router/dev/routes";
+import type { RouteConfig } from "@react-router/dev/routes";
+import { index, route } from "@react-router/dev/routes";
+import { getAddonRoutesConfig } from "@plone/react-router";
+import config from "@plone/registry";
+// eslint-disable-next-line import/no-unresolved
+import applyAddonConfiguration, { addonsInfo } from "../registry.loader";
 
-export default [index("routes/home.tsx")] satisfies RouteConfig;
+// applyAddonConfiguration(config);
+
+const routes: RouteConfig = [
+  index("routes/home.tsx", { id: "index" }),
+  ...getAddonRoutesConfig(config.routes || [], addonsInfo),
+];
+
+export default routes;
