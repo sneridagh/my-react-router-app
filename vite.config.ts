7dc88699a708a5c2f231ee9f1b9cddafa5676226
Victor Fernandez de Alba <sneridagh@gmail.com>
2025-02-24 13:31:06 +0100

    PoC using @plone/registry

---
+++ vite.config.ts
@@ -0,0 +1,14 @@
+import { reactRouter } from "@react-router/dev/vite";
+import tailwindcss from "@tailwindcss/vite";
+import { defineConfig } from "vite";
+import tsconfigPaths from "vite-tsconfig-paths";
+import { PloneRegistryVitePlugin } from "@plone/registry/vite-plugin";
+
+export default defineConfig({
+	plugins: [
+		PloneRegistryVitePlugin(),
+		tailwindcss(),
+		reactRouter(),
+		tsconfigPaths(),
+	],
+});
